059533385054e350532a69b354fbf2372481adb1
yuzid <yazid.fauzan9999@gmail.com>
2024-03-15 21:29:03 +0700

    tambah main baru

--- Proyek-2/main.cpp
+++ Proyek-2/main.cpp
@@ -1,5 +1,26 @@
 #include<stdio.h>
+#include"header.h"
+
 
 int main(){
-	puts("Hello WOrld");
+	int menu;
+	puts("\t\t\t\t\t\t Cryptwography ");
+	puts("");
+	puts("");
+	puts("1.Login\n2.Register\n3.Exit");
+	scanf("%d", &menu);
+	while (menu != 3) {
+
+		switch (menu) {
+		case 1:
+			login();
+			break;
+		}
+	case 2:
+		registrasi();
+		break;
+	case 3:
+		exit
+
+	}
 }
