6c14ddb3eae520d7cd4326b9c84428d7ed334068
Muhammad Reivan N M <reivnnaufl@gmail.com>
2024-05-13 08:59:06 +0700

    .

--- Proyek-2/Header.h
+++ Proyek-2/Header.h
@@ -18,14 +18,16 @@
 
 #define MAX_FILE_SIZE 1024
 
-#define MAXrng 50000
+    //#define MAXrng 50000 reivan
 //test
 
-typedef struct {
-    uint64_t privateKey;
-    uint64_t publicKey;
-    uint64_t product;
-}RSAkey;
+    /*
+    typedef struct {
+        uint64_t privateKey;
+        uint64_t publicKey;
+        uint64_t product;
+    }RSAkey;                    reivan
+    */
 
 typedef struct {
     char user[10][100];
@@ -41,37 +43,49 @@ void decryptToString(unsigned long long int cipher[], int length, unsigned long 
 // Fungsi untuk mengembalikan hasil dari modul pertama
 void firstmodul(unsigned long long int e, unsigned long long int n, char q[]);
 
-// Fungsi untuk melakukan pemangkatan dengan eksponensiasi cepat
-uint64_t modExpo(uint64_t base, uint64_t power, uint64_t divisor);
 
-// Fungsi untuk menghitung fungsi phi Euler
-uint64_t phi(uint64_t prime1, uint64_t prime2);
+    /*
+    // Fungsi untuk melakukan pemangkatan dengan eksponensiasi cepat
+    uint64_t modExpo(uint64_t base, uint64_t power, uint64_t divisor);
 
-// Fungsi untuk melakukan uji keprimaan Miller-Rabin
-bool millerRabinTest(uint64_t n, uint64_t i);
+    // Fungsi untuk menghitung fungsi phi Euler
+    uint64_t phi(uint64_t prime1, uint64_t prime2);
 
-// Fungsi untuk menguji apakah suatu bilangan prima
-bool isPrime(uint64_t number);
+    // Fungsi untuk melakukan uji keprimaan Miller-Rabin
+    bool millerRabinTest(uint64_t n, uint64_t i);
 
-// Fungsi untuk menghasilkan bilangan prima dalam rentang tertentu
-uint64_t genPrime(uint64_t lowerBound, uint64_t upperBound);
+    // Fungsi untuk menguji apakah suatu bilangan prima
+    bool isPrime(uint64_t number);
 
-// Fungsi untuk menghasilkan kunci privat
-uint64_t genPrivateKey(uint64_t totient);
+    // Fungsi untuk menghasilkan bilangan prima dalam rentang tertentu
+    uint64_t genPrime(uint64_t lowerBound, uint64_t upperBound);
 
-//function untuk mencari modular multiplicative inverse menggunakan extended euclidian alg
-//dan menghitung GCD(greatest common divisor)(FPB) menggunakan euclidian alg
-uint64_t euclidianAlg(uint64_t s, uint64_t l, int64_t prevRemainder, int64_t* t1, int64_t* t2);
+    // Fungsi untuk menghasilkan kunci privat
+    uint64_t genPrivateKey(uint64_t totient);
 
-//function euclidianAlg dengan validator dan tambahan program jika hasil t1 negatif 
-uint64_t modInverse(uint64_t s, uint64_t l);
+    //function untuk mencari modular multiplicative inverse menggunakan extended euclidian alg
+    //dan menghitung GCD(greatest common divisor)(FPB) menggunakan euclidian alg
+    uint64_t euclidianAlg(uint64_t s, uint64_t l, int64_t prevRemainder, int64_t* t1, int64_t* t2);
 
-//function yang mengenerate private key, public key, dan product (perkalian 2 bil prima)
-RSAkey genRSAkeys();
+    //function euclidianAlg dengan validator dan tambahan program jika hasil t1 negatif 
+    uint64_t modInverse(uint64_t s, uint64_t l);
 
-//procedure dekripsi file yang terenkripsi dan print isinya ke layar
-void fileDecrypt(LoginResult info);
+    //function yang mengenerate private key, public key, dan product (perkalian 2 bil prima)
+    RSAkey genRSAkeys();
 
+    //procedure dekripsi file yang terenkripsi dan print isinya ke layar
+    void fileDecrypt(LoginResult info);
+            
+            reivan
+    */
+
+void listFiles(const char* path, char filenames[][256], int* file_count);
+void displayFileContent(const char* path, const char* filename);
+
+void show_files(const char* directory_path);
+char* choose_file(const char* directory_path, int selected_file_index);
+
+void mainLogic();
 
 void recordHistory(char username[], char filename[]); 
 
